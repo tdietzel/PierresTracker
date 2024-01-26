21ccc0c588b1ca4b97f4dde5da770fb349c49445
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-26 09:14:36 -0800

    Add complete Test #5 with refactor for Vendor

--- PierresTracker.Tests/ModelTests/VendorTests.cs
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -12,31 +12,47 @@ namespace PierresTracker.Tests
       Vendor.ResetNextId();
     }
 
-    [TestMethod]
+    [TestMethod] // Test #1
     public void VendorConstructor_CreatesAVendorConstructor_Vendor()
     {
-      Vendor newVendor = new Vendor("");
+      Vendor newVendor = new Vendor("", "");
+
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
-    [TestMethod]
+    [TestMethod] // Test #2
     public void GetsVendorName_ReturnsName_String()
     {
       string name = "Suzie's Cafe";
-      Vendor newVendor = new Vendor("Suzie's Cafe");
+      Vendor newVendor = new Vendor("Suzie's Cafe", "");
       string result = newVendor.Name;
+
       Assert.AreEqual(name, result);
     }
-    [TestMethod]
+    [TestMethod]  // Test #5
+    public void GetsVendorDescription_ReturnsDescription_String()
+    {
+      string name= "Suzie's Cafe";
+      string description = "The Cafe on 3rd street in testing city";
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Description;
+
+      Assert.AreEqual(description, result);
+    }
+    [TestMethod]  // Test #3
     public void GetsVendorId_ReturnsId_Int()
     {
-      Vendor newVendor = new Vendor("Suzie's Cafe");
+      string description = "The Cafe on 3rd street in testing city";
+      Vendor newVendor = new Vendor("Suzie's Cafe", description);
+
       Assert.AreEqual(1, newVendor.Id);
     }
-    [TestMethod]
+    [TestMethod]  // Test #4
     public void AddsVendorToList_ReturnsVendorList_List()
     {
-      Vendor newVendor = new Vendor("Suzie's Cafe");
+      string description = "The Cafe on 3rd street in testing city";
+      Vendor newVendor = new Vendor("Suzie's Cafe", description);
       List<Vendor> allVendors = new List<Vendor> { newVendor };
+
       Assert.AreEqual(newVendor, allVendors[0]);
     }
   }
--- PierresTracker/Models/Vendor.cs
+++ PierresTracker/Models/Vendor.cs
@@ -6,12 +6,14 @@ namespace PierresTracker.Models
   {
     public static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; set; }
     private static int nextId = 0;
 
-    public Vendor(string name)
+    public Vendor(string name, string description)
     {
       Name = name;
+      Description = description;
       Id = nextId++;
       _instances.Add(this);
     }
