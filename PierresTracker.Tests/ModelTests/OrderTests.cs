029562ae3310cdd9b62de5d83f75741ecf11b2d6
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-26 09:33:43 -0800

    Add complete Test #1 for Order

--- PierresTracker.Tests/ModelTests/OrderTests.cs
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -6,6 +6,11 @@ namespace PierresTracker.Tests
   [TestClass]
   public class OrderTests
   {
-
+    [TestMethod] // Test #1
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
